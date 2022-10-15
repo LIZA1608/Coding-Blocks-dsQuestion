593e5e96054823d779c45e3eb544d6db6fb0f710
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2022-10-15 11:32:42 +0530

    Update Arrays-Spiral Print Clockwise.cpp

--- Arrays-Spiral Print Clockwise.cpp
+++ Arrays-Spiral Print Clockwise.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 int main()
 {
-	int n;
-    cin>>n;
-    int a[n][n];
+	int n,m;
+    cin>>n>>m;
+    int a[n][m];
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+        for(int j=0;j<m;j++){
             cin>>a[i][j];
         }
     }
@@ -17,29 +17,29 @@ int main()
    int top=0;
    int down=n-1;
    int left=0;
-   int right=n-1;
+   int right=m-1;
    while(top<=down && left<=right){
        if(dir==0){
            for(int i=left;i<=right;i++){
-               cout<<a[top][i]<<",";
+               cout<<a[top][i]<<", ";
            }
            top++;
        }
        if(dir==1){
            for(int i=top;i<=down;i++){
-               cout<<a[i][right]<<",";
+               cout<<a[i][right]<<", ";
            }
            right--;
        }
        if(dir ==2){
            for(int i=right;i>=left;i--){
-               cout<<a[down][i]<<",";
+               cout<<a[down][i]<<", ";
            }
            down--;
        }
        if(dir ==3){
            for(int i=down;i>=top;i--){
-               cout<<a[i][left]<<",";
+               cout<<a[i][left]<<", ";
            }
            left++;
        }
