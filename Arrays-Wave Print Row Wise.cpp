6d51e3b7f9909345aef80eead64cf9609d7cdd44
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2022-10-15 10:55:43 +0530

    Update Arrays-Wave Print Row Wise.cpp

--- Arrays-Wave Print Row Wise.cpp
+++ Arrays-Wave Print Row Wise.cpp
@@ -15,15 +15,15 @@ int main()
     for(int i=0;i<n;i++){
         if(i%2==0){
             for(int j=0;j<n;j++){
-                cout<<a[i][j]<<" ";
+                cout<<a[i][j]<<",";
             }
         }
         else{
             for(int j=n-1;j>=0;j--){
-                cout<<a[i][j]<<" ";
+                cout<<a[i][j]<<",";
             }
         }
-        cout<<endl;
+       // cout<<endl;
     }
     cout<<"END";
   	return 0;
